cc5dbdb4e7ea0205d56c8e6d4bc0b3a063fa20b0
siddharth <angular.dev2015@gmail.com>
2016-03-30 01:29:20 +0530

    first commit

--- public_html/main/home/js/homeCtrl.js
+++ public_html/main/home/js/homeCtrl.js
@@ -1,6 +1,11 @@
-(function(){
+(function () {
     'use strict';
-   angular.module('networking').controller('HomeCtrl',[function(){
-           var vm=this;
-   }]); 
+    angular.module('networking').controller('HomeCtrl', ['exploreGraph', function (exploreGraph) {
+            var promise = exploreGraph.data();
+            promise.then(function (data) {
+                console.log(data.data);
+                 exploreGraph.create(data.data);
+            })
+           
+        }]);
 })();
